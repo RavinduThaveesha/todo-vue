1c6891fcb7e42a450add9cbd323e84285880b8e6
hexosysravindu <ravindu@msd-my.com>
2019-09-24 18:10:27 +0800

    add todos, todo item components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,50 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <Todos v-bind:todos="todos"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todos from './components/Todos';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Todos
+  },
+  data() {
+      return {
+          todos: [
+              {
+                  id: 1,
+                  text: "Bank Appointment",
+                  isCompleted: true
+              },
+              {
+                  id: 2,
+                  text: "Team Meeting",
+                  isCompleted: false
+              },
+              {
+                  id: 3,
+                  text: "Dinner with Amy",
+                  isCompleted: false
+              }
+          ]
+      }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+}
+
+body {
+    font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
+    line-height: 1.4;
 }
 </style>
